c10c3e0a772f5f7a8347ba76640554a49827b7c2
Ronald Arias <104944902+uisweb@users.noreply.github.com>
2022-09-10 17:37:31 -0500

    Update asgi.py

--- Taller_7/asgi.py
+++ Taller_7/asgi.py
@@ -1,1 +1,14 @@
+"""
+ASGI config for Taller_7 project.
+It exposes the ASGI callable as a module-level variable named ``application``.
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
 
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Taller_7.settings')
+
+application = get_asgi_application()
